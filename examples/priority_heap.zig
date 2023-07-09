debfe98103511d83aa10a20714db315c4b5df5ed
Igor Anić <igor.anic@gmail.com>
2023-07-09 18:02:16 +0200

    compare pairing heap with priority queue

--- examples/priority_heap.zig
+++ examples/priority_heap.zig
@@ -7,6 +7,7 @@ const assert = std.debug.assert;
 const print = std.debug.print;
 
 const Heap = zds.pairing_heap.Heap(Timer, void, Timer.less);
+const number_of_timers = 10_000_000;
 
 const Timer = struct {
     const Self = @This();
@@ -17,16 +18,49 @@ const Timer = struct {
     fn less(_: void, self: *Timer, other: *Timer) bool {
         return self.expires < other.expires;
     }
+
+    fn compare(_: void, self: *Timer, other: *Timer) std.math.Order {
+        return std.math.order(self.expires, other.expires);
+    }
 };
 
+// benchmark:
+//   pairing heap
+//           16016.835 ms total
+//           0.005 ms alloc
+//           157.034 ms init
+//           15859.796 ms deleteMin
+//           bytes used 480000044, per node: 48
+//   std priority queue
+//           7509.926 ms total
+//           0.014 ms alloc
+//           247.937 ms init
+//           7261.975 ms deleteMin
+//           bytes used 787087264, per node: 78
+//
 pub fn main() !void {
     const GPA = std.heap.GeneralPurposeAllocator(.{});
     var gpa: GPA = .{};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
 
+    var arena = std.heap.ArenaAllocator.init(alloc);
+    defer arena.deinit();
+
+    print("pairing heap\n", .{});
+    try pairing_heap(arena.allocator());
+    print("\tbytes used {d}, per node: {d}\n", .{ arena.queryCapacity(), arena.queryCapacity() / number_of_timers });
+    assert(arena.reset(.free_all));
+
+    print("std priority queue\n", .{});
+    try std_priority_queue(arena.allocator());
+    print("\tbytes used {d}, per node: {d}\n", .{ arena.queryCapacity(), arena.queryCapacity() / number_of_timers });
+    assert(arena.reset(.free_all));
+}
+
+fn pairing_heap(alloc: mem.Allocator) !void {
     const before_alloc = try Instant.now();
-    var timers = try alloc.alloc(Timer, 10_000_000);
+    var timers = try alloc.alloc(Timer, number_of_timers);
     defer alloc.free(timers);
 
     var expire_times = try alloc.alloc(usize, timers.len);
@@ -54,8 +88,49 @@ pub fn main() !void {
         i += 1;
     }
     const after_all = try Instant.now();
-    print("{d:.3} ms total\n", .{@as(f64, @floatFromInt(after_all.since(before_alloc))) / 1e6});
-    print("{d:.3} ms alloc\n", .{@as(f64, @floatFromInt(before_init.since(before_alloc))) / 1e6});
-    print("{d:.3} ms init\n", .{@as(f64, @floatFromInt(after_init.since(before_init))) / 1e6});
-    print("{d:.3} ms deleteMin\n", .{@as(f64, @floatFromInt(after_all.since(after_init))) / 1e6});
+    print("\t{d:.3} ms total\n", .{@as(f64, @floatFromInt(after_all.since(before_alloc))) / 1e6});
+    print("\t{d:.3} ms alloc\n", .{@as(f64, @floatFromInt(before_init.since(before_alloc))) / 1e6});
+    print("\t{d:.3} ms init\n", .{@as(f64, @floatFromInt(after_init.since(before_init))) / 1e6});
+    print("\t{d:.3} ms deleteMin\n", .{@as(f64, @floatFromInt(after_all.since(after_init))) / 1e6});
+}
+
+fn std_priority_queue(alloc: mem.Allocator) !void {
+    const Queue = std.PriorityQueue(*Timer, void, Timer.compare);
+    var queue = Queue.init(alloc, {});
+    defer queue.deinit();
+
+    const before_alloc = try Instant.now();
+    try queue.ensureTotalCapacity(number_of_timers);
+    var timers = try alloc.alloc(Timer, number_of_timers);
+    defer alloc.free(timers);
+
+    var expire_times = try alloc.alloc(usize, timers.len);
+    defer alloc.free(expire_times);
+
+    const before_init = try Instant.now();
+    for (expire_times, 0..) |*k, i| k.* = i;
+    var prng = std.rand.DefaultPrng.init(0);
+    prng.random().shuffle(usize, expire_times);
+
+    for (expire_times, 0..) |e, i| {
+        var timer = &timers[i];
+        timer.* = .{
+            .expires = e,
+            .heap = .{},
+        };
+        try queue.add(timer);
+    }
+    const after_init = try Instant.now();
+
+    var i: usize = 0;
+    while (queue.removeOrNull()) |timer| {
+        assert(timer.expires == i);
+        i += 1;
+    }
+
+    const after_all = try Instant.now();
+    print("\t{d:.3} ms total\n", .{@as(f64, @floatFromInt(after_all.since(before_alloc))) / 1e6});
+    print("\t{d:.3} ms alloc\n", .{@as(f64, @floatFromInt(before_init.since(before_alloc))) / 1e6});
+    print("\t{d:.3} ms init\n", .{@as(f64, @floatFromInt(after_init.since(before_init))) / 1e6});
+    print("\t{d:.3} ms deleteMin\n", .{@as(f64, @floatFromInt(after_all.since(after_init))) / 1e6});
 }
--- readme.md
+++ readme.md
@@ -1,28 +1,35 @@
 ### Some notable data structures from Zig Standard Library
 
-General purpose hash with preserved insertion order.
+General purpose hash with preserved insertion order:
 [array hash map](https://github.com/ziglang/zig/blob/master/lib/std/array_hash_map.zig)  
 
+A contiguous, growable list of items in memory.
 [array list](https://github.com/ziglang/zig/blob/master/lib/std/array_list.zig)  
 
+<!--
 A structure with an array and a length, that can be used as a slice.
 [bounded array](https://github.com/ziglang/zig/blob/master/lib/std/bounded_array.zig)  
-
+-->
 
 [fifo](https://github.com/ziglang/zig/blob/master/lib/std/fifo.zig)  
 
+Hash map (disctionary): 
 [hash map](https://github.com/ziglang/zig/blob/master/lib/std/hash_map.zig)  
 
+Data oriented struct store, array for each struct filed: 
 [multi array list](https://github.com/ziglang/zig/blob/master/lib/std/multi_array_list.zig)
 
 [priority queue](https://github.com/ziglang/zig/blob/master/lib/std/priority_queue.zig)
 
 [priority dequeue](https://github.com/ziglang/zig/blob/master/lib/std/priority_dequeue.zig)
 
+[segmented list](https://github.com/ziglang/zig/blob/master/lib/std/segmented_list.zig)
+
 [singly linked list](https://github.com/ziglang/zig/blob/master/lib/std/linked_list.zig)  
 
 [tail queue](https://github.com/ziglang/zig/blob/6f766fbf008160150a6a164c2dae5a6ee2a5543c/lib/std/linked_list.zig#L160)  
 
+[treap](https://github.com/ziglang/zig/blob/master/lib/std/treap.zig)
 
 ### Pairing Heap
 
